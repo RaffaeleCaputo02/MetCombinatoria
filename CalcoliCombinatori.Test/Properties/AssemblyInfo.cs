66a3b4a45ef3ff68649f05e6fe931651ad196943
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-06 09:12:30 +0100

    es finito e funzionante

---
+++ CalcoliCombinatori.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcoliCombinatori.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcoliCombinatori.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a547852-6c45-4650-bbf1-c5b226a6bfe2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EquazioniLibrary/CalcoliCombinatori.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazioniLibrary
+{
+    public static class CalcoliCombinatori
+    {
+        public static long Fattoriale(int num)
+        {
+            if (num < 0)
+            {
+                num = num * (-1);
+            }
+            long fattoriale = 1;
+            for(int i=num; i>0; i--)
+            {
+                fattoriale = fattoriale * i;
+            }
+
+            return fattoriale;
+        }
+    }
+}
